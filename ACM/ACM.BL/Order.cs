1e351bae65afdeb64a1d7e654fb4cce5d886c076
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2018-08-04 14:15:39 +0200

    3_Creating Methods of a class

---
+++ ACM/ACM.BL/Order.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Order
+    {
+        public Order()
+        {
+
+        }
+
+        public Order(int orderId)
+        {
+            this.OrderId = orderId;
+        }
+
+        public int OrderId { get; private set; }
+
+        public string ShippingAddress { get; set; }
+
+        // "DateTimeOffset" is very usefull if date is in different time zone.
+        // it is easier to be used than normalizing all date to "UTC".
+        // we use "nullable" time to know if the date is set or not.
+        public DateTimeOffset? OrderDate { get; set; }
+
+        public Order Retrieve(int productId)
+        {
+            return new Order();
+        }
+
+        public List<Order> Retrieve()
+        {
+            return new List<Order>();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            bool isValid = true;
+
+            if(OrderDate == null)
+            {
+                isValid = false;
+            }
+
+            return isValid;
+        }
+
+    }
+}
---
+++ ACM/ACM.BL/Product.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(int productId)
+        {
+            this.ProductId = productId;
+        }
+
+        public int ProductId { get; private set; }
+
+        public string ProductDescription { get; set; }
+
+        public string ProductName { get; set; }
+        
+        // question mark deckares a "nullable type".
+        // "nullable type" is a "value type" like int, string, and ....
+        // "nullable type" allows variable to have "value" or a "null".
+        // it is used to distinguish between "Not Set" and "zero".
+        public decimal? CurrentPrice { get; set; }
+
+        public Product Retrieve(int productId)
+        {
+            return new Product();
+        }
+
+        public List<Product> Retrieve()
+        {
+            return new List<Product>();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool validate()
+        {
+            bool isValid = true;
+
+            if(string.IsNullOrWhiteSpace(ProductName))
+            {
+                isValid = false;
+            }
+
+            if(CurrentPrice == null)
+            {
+                isValid = false;
+            }
+
+            return isValid;
+        }
+
+    }
+}
