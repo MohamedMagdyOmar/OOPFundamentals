84206ad891ac0fb04fda59e2dc8eff2550120d8a
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2018-08-06 12:43:18 +0200

    1_collaboration_relationship

--- ACM/ACM.BL/CustomerRepository.cs
+++ ACM/ACM.BL/CustomerRepository.cs
@@ -8,13 +8,21 @@ namespace ACM.BL
 {
     public class CustomerRepository
     {
-        public bool Save()
+        // the method "save" takes an instance of the "customer" class
+        // then uses info from this instance to save info to the DB (serialize this "customer" class).
+        // this also a "collaboration" relationship, because this class
+        // "uses a" instance of this class.
+        public bool Save(Customer customer)
         {
             return true;
         }
 
         public Customer Retrieve(int customerId)
         {
+            // here we create new instance of the customer class
+            // then populate it with info from DB
+            // so this "Collaboration" relationship, because
+            // this class "uses a" the "customer" class
             var customer = new Customer(customerId);
             if (customerId == 1)
             {
